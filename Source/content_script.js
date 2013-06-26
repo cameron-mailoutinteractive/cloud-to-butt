5871d5dc1939297add5917a11388773b307e35d9
Cameron D <cameron@mailoutinteractive.com>
2013-06-26 16:00:44 -0600

    Update content_script.js

--- Source/content_script.js
+++ Source/content_script.js
@@ -31,10 +31,10 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bMy Butt\b/g, "Donuts");
+	v = v.replace(/\bMy butt\b/g, "Donuts");
+	v = v.replace(/\bmy Butt\b/g, "donuts");
+	v = v.replace(/\bmy butt\b/g, "donuts");
 	
 	textNode.nodeValue = v;
 }
